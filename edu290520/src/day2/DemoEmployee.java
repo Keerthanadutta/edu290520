5721a66098b851226f6701285e833ca5c062f4ed
dutta <dutta@10.0.0.16>
2020-06-03 17:24:46 +0930

    Push

---
+++ edu290520/src/day2/DemoEmployee.java
@@ -0,0 +1,21 @@
+package day2;
+
+public class DemoEmployee {
+
+	public static void main(String[] args) {
+		// Declaring a variable
+          Employee keerthi;
+          
+       // Initiation and Initialization
+          keerthi = new Employee ();
+          
+          keerthi.salary = 8000;
+          keerthi.bonus =900;
+          
+          keerthi.calculatesalary ();
+        		  
+          
+          
+	}
+
+}
---
+++ edu290520/src/day2/Employee.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class Employee {
+
+	int salary;
+	int bonus;
+	
+	void calculatesalary() {
+	
+	int totalsalary = salary+bonus;
+ 	
+	System.out.println("Total salry : " + totalsalary);
+	
+}
+}
